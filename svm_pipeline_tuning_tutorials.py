d3e46403752ad82b2f9df862fc66a0ded0e0befa
c60evaporator <c60evaporator@gmail.com>
2021-05-30 21:10:42 +0900

    初期検証曲線の範囲を広めに統一

--- svm_pipeline_tuning_tutorials.py
+++ svm_pipeline_tuning_tutorials.py
@@ -31,50 +31,11 @@ print(f'average_score={np.mean(scores)}')
 # gammaのデフォルト値を表示
 print(f'gamma = {1 /(X.shape[1] * StandardScaler().fit_transform(X).var())}')
 
-# %% 手順2) パラメータ種類と範囲の選択
-from sklearn.model_selection import validation_curve
-import matplotlib.pyplot as plt
-cv_params = {'svm__gamma': [0.01, 0.03, 0.1, 0.3, 1, 3, 10],
-             'svm__C': [0.1, 0.3, 1, 3, 10]}
-# 検証曲線のプロット（パラメータ毎にプロット）
-for i, (k, v) in enumerate(cv_params.items()):
-    train_scores, valid_scores = validation_curve(estimator=model,
-                                                  X=X, y=y,
-                                                  param_name=k,
-                                                  param_range=v,
-                                                  cv=cv, scoring=scoring,
-                                                  n_jobs=-1)
-    # 学習データに対するスコアの平均±標準偏差を算出
-    train_mean = np.mean(train_scores, axis=1)
-    train_std  = np.std(train_scores, axis=1)
-    train_center = train_mean
-    train_high = train_mean + train_std
-    train_low = train_mean - train_std
-    # テストデータに対するスコアの平均±標準偏差を算出
-    valid_mean = np.mean(valid_scores, axis=1)
-    valid_std  = np.std(valid_scores, axis=1)
-    valid_center = valid_mean
-    valid_high = valid_mean + valid_std
-    valid_low = valid_mean - valid_std
-    # training_scoresをプロット
-    plt.plot(v, train_center, color='blue', marker='o', markersize=5, label='training score')
-    plt.fill_between(v, train_high, train_low, alpha=0.15, color='blue')
-    # validation_scoresをプロット
-    plt.plot(v, valid_center, color='green', linestyle='--', marker='o', markersize=5, label='validation score')
-    plt.fill_between(v, valid_high, valid_low, alpha=0.15, color='green')
-    # スケールを'log'に（線形なパラメータは'linear'にするので注意）
-    plt.xscale('log')
-    # 軸ラベルおよび凡例の指定
-    plt.xlabel(k)  # パラメータ名を横軸ラベルに
-    plt.ylabel(scoring)  # スコア名を縦軸ラベルに
-    plt.legend(loc='lower right')  # 凡例
-    # グラフを描画
-    plt.show()
-# %% 手順2) パラメータ種類と範囲の選択（範囲を広げる）
+# %% 手順2) パラメータ種類と範囲の選択（svm_tuning_tutorials.pyより広めに範囲をとる）
 from sklearn.model_selection import validation_curve
 import matplotlib.pyplot as plt
 cv_params = {'svm__gamma': [0.0001, 0.001, 0.01, 0.03, 0.1, 0.3, 1, 3, 10, 100, 1000],
-             'svm__C': [0.001, 0.01, 0.1, 0.3, 1, 3, 10, 100, 1000, 10000, 100000, 1000000]}
+             'svm__C': [0.001, 0.01, 0.1, 0.3, 1, 3, 10, 100, 1000]}
 # 検証曲線のプロット（パラメータ毎にプロット）
 for i, (k, v) in enumerate(cv_params.items()):
     train_scores, valid_scores = validation_curve(estimator=model,
