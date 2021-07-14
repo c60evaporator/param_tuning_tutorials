0a6fdbe88893dbc23a8b7cfa3348d0d8ddee8de0
c60evaporator <c60evaporator@gmail.com>
2021-07-15 00:44:05 +0900

    LGBM Optunaまで実装

---
+++ lgbm_tuning_tutorials.py
@@ -0,0 +1,274 @@
+# %% データの読込
+import pandas as pd
+import time
+df_osaka = pd.read_csv(f'./sample_data/osaka_metropolis_english.csv')
+OBJECTIVE_VARIALBLE = 'approval_rate'  # 目的変数
+USE_EXPLANATORY = ['2_between_30to60', '3_male_ratio', '5_household_member', 'latitude']  # 説明変数
+y = df_osaka[OBJECTIVE_VARIALBLE].values  # 目的変数をndarray化
+X = df_osaka[USE_EXPLANATORY].values  # 説明変数をndarray化
+# データを表示
+df_osaka[USE_EXPLANATORY + [OBJECTIVE_VARIALBLE]]
+
+# %% チューニング前のモデル
+from custom_scatter_plot import regplot
+from lightgbm import LGBMRegressor
+from sklearn.model_selection import KFold
+# 乱数シード
+seed = 42
+# モデル作成
+model = LGBMRegressor(boosting_type='gbdt', objective='regression',
+                      random_state=seed, n_estimators=10000)  # チューニング前のモデル(booster以外のパラメータ指定しない)
+# 学習時fitパラメータ指定
+fit_params = {'verbose': 0,  # 学習中のコマンドライン出力
+              'early_stopping_rounds': 10,  # 学習時、評価指標がこの回数連続で改善しなくなった時点でストップ
+              'eval_metric': 'rmse',  # early_stopping_roundsの評価指標
+              'eval_set': [(X, y)]  # early_stopping_roundsの評価指標算出用データ
+              }
+# クロスバリデーションして決定境界を可視化
+cv = KFold(n_splits=3, shuffle=True, random_state=seed)  # KFoldでクロスバリデーション分割指定
+# regplot.regression_heat_plot(model, USE_EXPLANATORY, OBJECTIVE_VARIALBLE, df_osaka,
+#                              pair_sigmarange = 0.5, rounddigit_x1=3, rounddigit_x2=3,
+#                              cv=cv, display_cv_indices=0,
+#                              fit_params=fit_params)
+
+# %% 手順1) チューニング前の評価指標算出
+from sklearn.model_selection import cross_val_score
+import numpy as np
+X = df_osaka[USE_EXPLANATORY].values  
+y = df_osaka[OBJECTIVE_VARIALBLE]  # 目的変数をndarray化
+scoring = 'neg_mean_squared_error'  # 評価指標をRMSEに指定
+# クロスバリデーションで評価指標算出
+scores = cross_val_score(model, X, y, cv=cv,
+                         scoring=scoring, n_jobs=-1, fit_params=fit_params)
+print(f'scores={scores}')
+print(f'average_score={np.mean(scores)}')
+
+# %% 手順2) パラメータ種類と範囲の選択
+from sklearn.model_selection import validation_curve
+import matplotlib.pyplot as plt
+cv_params = {'reg_alpha': [0, 0.0001, 0.0003, 0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 3, 10],
+             'reg_lambda': [0, 0.0001, 0.0003, 0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 3, 10],
+             'num_leaves': [2, 4, 8, 16, 32, 64, 96, 128, 160, 192, 224, 256],
+             'colsample_bytree': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+             'subsample': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+             'subsample_freq': [0, 1, 2, 3, 4, 5, 6, 7],
+             'min_child_samples': [0, 2, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+             }
+param_scales = {'reg_alpha': 'log',
+                'reg_lambda': 'log',
+                'num_leaves': 'linear',
+                'colsample_bytree': 'linear',
+                'subsample': 'linear',
+                'subsample_freq': 'linear',
+                'min_child_samples': 'linear'
+                }
+# 検証曲線のプロット（パラメータ毎にプロット）
+for i, (k, v) in enumerate(cv_params.items()):
+    train_scores, valid_scores = validation_curve(estimator=model,
+                                                  X=X, y=y,
+                                                  param_name=k,
+                                                  param_range=v,
+                                                  fit_params=fit_params,
+                                                  cv=cv, scoring=scoring,
+                                                  n_jobs=-1)
+    # 学習データに対するスコアの平均±標準偏差を算出
+    train_mean = np.mean(train_scores, axis=1)
+    train_std  = np.std(train_scores, axis=1)
+    train_center = train_mean
+    train_high = train_mean + train_std
+    train_low = train_mean - train_std
+    # テストデータに対するスコアの平均±標準偏差を算出
+    valid_mean = np.mean(valid_scores, axis=1)
+    valid_std  = np.std(valid_scores, axis=1)
+    valid_center = valid_mean
+    valid_high = valid_mean + valid_std
+    valid_low = valid_mean - valid_std
+    # training_scoresをプロット
+    plt.plot(v, train_center, color='blue', marker='o', markersize=5, label='training score')
+    plt.fill_between(v, train_high, train_low, alpha=0.15, color='blue')
+    # validation_scoresをプロット
+    plt.plot(v, valid_center, color='green', linestyle='--', marker='o', markersize=5, label='validation score')
+    plt.fill_between(v, valid_high, valid_low, alpha=0.15, color='green')
+    # スケールを'log'に（線形なパラメータは'linear'にするので注意）
+    plt.xscale(param_scales[k])
+    # 軸ラベルおよび凡例の指定
+    plt.xlabel(k)  # パラメータ名を横軸ラベルに
+    plt.ylabel(scoring)  # スコア名を縦軸ラベルに
+    plt.legend(loc='lower right')  # 凡例
+    # グラフを描画
+    plt.show()
+
+# %% 手順2) パラメータ種類と範囲の選択（min_child_samplesのデフォルト値を20→5に、num_leavesを31→2に変更）
+from sklearn.model_selection import validation_curve
+import matplotlib.pyplot as plt
+model.set_params(min_child_samples=5, num_leaves=2)
+# 検証曲線のプロット（パラメータ毎にプロット）
+for i, (k, v) in enumerate(cv_params.items()):
+    train_scores, valid_scores = validation_curve(estimator=model,
+                                                  X=X, y=y,
+                                                  param_name=k,
+                                                  param_range=v,
+                                                  fit_params=fit_params,
+                                                  cv=cv, scoring=scoring,
+                                                  n_jobs=-1)
+    # 学習データに対するスコアの平均±標準偏差を算出
+    train_mean = np.mean(train_scores, axis=1)
+    train_std  = np.std(train_scores, axis=1)
+    train_center = train_mean
+    train_high = train_mean + train_std
+    train_low = train_mean - train_std
+    # テストデータに対するスコアの平均±標準偏差を算出
+    valid_mean = np.mean(valid_scores, axis=1)
+    valid_std  = np.std(valid_scores, axis=1)
+    valid_center = valid_mean
+    valid_high = valid_mean + valid_std
+    valid_low = valid_mean - valid_std
+    # training_scoresをプロット
+    plt.plot(v, train_center, color='blue', marker='o', markersize=5, label='training score')
+    plt.fill_between(v, train_high, train_low, alpha=0.15, color='blue')
+    # validation_scoresをプロット
+    plt.plot(v, valid_center, color='green', linestyle='--', marker='o', markersize=5, label='validation score')
+    plt.fill_between(v, valid_high, valid_low, alpha=0.15, color='green')
+    # スケールを'log'に（線形なパラメータは'linear'にするので注意）
+    plt.xscale(param_scales[k])
+    # 軸ラベルおよび凡例の指定
+    plt.xlabel(k)  # パラメータ名を横軸ラベルに
+    plt.ylabel(scoring)  # スコア名を縦軸ラベルに
+    plt.legend(loc='lower right')  # 凡例
+    # グラフを描画
+    plt.show()
+
+# %% 手順3＆4) パラメータ選択＆クロスバリデーション（グリッドサーチ）
+from sklearn.model_selection import GridSearchCV
+start = time.time()
+# 最終的なパラメータ範囲（1152通り）
+cv_params = {'reg_alpha': [0.0001, 0.003, 0.1],
+             'reg_lambda': [0.0001, 0.1],
+             'num_leaves': [2, 3, 4, 6],
+             'colsample_bytree': [0.4, 0.7, 1.0],
+             'subsample': [0.4, 1.0],
+             'subsample_freq': [0, 7],
+             'min_child_samples': [0, 2, 5, 10]
+             }
+# グリッドサーチのインスタンス作成
+gridcv = GridSearchCV(model, cv_params, cv=cv,
+                      scoring=scoring, n_jobs=-1)
+# グリッドサーチ実行（学習実行）
+gridcv.fit(X, y, **fit_params)
+# 最適パラメータの表示と保持
+best_params = gridcv.best_params_
+best_score = gridcv.best_score_
+print(f'最適パラメータ {best_params}\nスコア {best_score}')
+print(f'所要時間{time.time() - start}秒')
+
+# %% 手順3＆4) パラメータ選択＆クロスバリデーション（ランダムサーチ）
+from sklearn.model_selection import RandomizedSearchCV
+start = time.time()
+# パラメータの種類と密度をグリッドサーチのときより増やす
+cv_params = {'reg_alpha': [0.0001, 0.0003, 0.001, 0.003, 0.01, 0.03, 0.1],
+             'reg_lambda': [0.0001, 0.0003, 0.001, 0.003, 0.01, 0.03, 0.1],
+             'num_leaves': [2, 3, 4, 5, 6],
+             'colsample_bytree': [0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+             'subsample': [0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+             'subsample_freq': [0, 1, 2, 3, 4, 5, 6, 7],
+             'min_child_samples': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+             }
+# ランダムサーチのインスタンス作成
+randcv = RandomizedSearchCV(model, cv_params, cv=cv,
+                            scoring=scoring, random_state=seed,
+                            n_iter=1000, n_jobs=-1)
+# ランダムサーチ実行（学習実行）
+randcv.fit(X, y, **fit_params)
+# 最適パラメータの表示と保持
+best_params = randcv.best_params_
+best_score = randcv.best_score_
+print(f'最適パラメータ {best_params}\nスコア {best_score}')
+print(f'所要時間{time.time() - start}秒')
+
+# %% 手順3＆4 パラメータ選択＆クロスバリデーション（BayesianOptimization対数軸でベイズ最適化）
+from bayes_opt import BayesianOptimization
+start = time.time()
+# パラメータ範囲（Tupleで範囲選択）
+bayes_params = {'reg_alpha': (0.0001, 0.1),
+                'reg_lambda': (0.0001, 0.1),
+                'num_leaves': (2, 6),
+                'colsample_bytree': (0.4, 1.0),
+                'subsample': (0.4, 1.0),
+                'subsample_freq': (0, 7),
+                'min_child_samples': (0, 10)
+                }
+# 対数スケールパラメータを対数化
+param_scales = {'reg_alpha': 'log',
+                'reg_lambda': 'log',
+                'num_leaves': 'linear',
+                'colsample_bytree': 'linear',
+                'subsample': 'linear',
+                'subsample_freq': 'linear',
+                'min_child_samples': 'linear'
+                }
+bayes_params_log = {k: (np.log10(v[0]), np.log10(v[1])) if param_scales[k] == 'log' else v for k, v in bayes_params.items()}
+# 整数型パラメータを指定
+int_params = ['num_leaves', 'subsample_freq', 'min_child_samples']
+
+# ベイズ最適化時の評価指標算出メソッド(引数が多いので**kwargsで一括読込)
+def bayes_evaluate(**kwargs):
+    params = kwargs
+    # 対数スケールパラメータは10のべき乗をとる
+    params = {k: np.power(10, v) if param_scales[k] == 'log' else v for k, v in params.items()}
+    # 整数型パラメータを整数化
+    params = {k: round(v) if k in int_params else v for k, v in params.items()}
+    # モデルにパラメータ適用
+    model.set_params(**params)
+    # cross_val_scoreでクロスバリデーション
+    scores = cross_val_score(model, X, y, cv=cv,
+                             scoring=scoring, fit_params=fit_params, n_jobs=-1)
+    val = scores.mean()
+    return val
+
+# ベイズ最適化を実行
+bo = BayesianOptimization(bayes_evaluate, bayes_params_log, random_state=seed)
+bo.maximize(init_points=20, n_iter=230, acq='ei')
+# 最適パラメータとスコアを取得
+best_params = bo.max['params']
+best_score = bo.max['target']
+# 対数スケールパラメータは10のべき乗をとる
+best_params = {k: np.power(10, v) if param_scales[k] == 'log' else v for k, v in best_params.items()}
+# 整数型パラメータを整数化
+best_params = {k: round(v) if k in int_params else v for k, v in best_params.items()}
+# 最適パラメータを表示
+print(f'最適パラメータ {best_params}\nスコア {best_score}')
+print(f'所要時間{time.time() - start}秒')
+
+# %% 手順3＆4) パラメータ選択＆クロスバリデーション（optunaでベイズ最適化）
+import optuna
+start = time.time()
+# ベイズ最適化時の評価指標算出メソッド
+def bayes_objective(trial):
+    params = {
+        'reg_alpha': trial.suggest_float('reg_alpha', 0.0001, 0.1, log=True),
+        'reg_lambda': trial.suggest_float('reg_lambda', 0.0001, 0.1, log=True),
+        'num_leaves': trial.suggest_int('num_leaves', 2, 6),
+        'colsample_bytree': trial.suggest_float('colsample_bytree', 0.4, 1.0),
+        'subsample': trial.suggest_float('subsample', 0.4, 1.0),
+        'subsample_freq': trial.suggest_int('subsample_freq', 0, 7),
+        'min_child_samples': trial.suggest_int('min_child_samples', 0, 10)
+    }
+    # モデルにパラメータ適用
+    model.set_params(**params)
+    # cross_val_scoreでクロスバリデーション
+    scores = cross_val_score(model, X, y, cv=cv,
+                             scoring=scoring, fit_params=fit_params, n_jobs=-1)
+    val = scores.mean()
+    return val
+
+# ベイズ最適化を実行
+study = optuna.create_study(direction='maximize',
+                            sampler=optuna.samplers.TPESampler(seed=seed))
+study.optimize(bayes_objective, n_trials=600)
+
+# 最適パラメータの表示と保持
+best_params = study.best_trial.params
+best_score = study.best_trial.value
+print(f'最適パラメータ {best_params}\nスコア {best_score}')
+print(f'所要時間{time.time() - start}秒')
+# %%
